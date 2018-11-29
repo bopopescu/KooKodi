2549395bf406aac79adea2daa311768267b4b6fd
Nathaniel <nathanielvo17@ou.edu>
2018-11-29 16:37:33 -0600

    Commit Test

---
+++ test/userdata/addon_data/plugin.video.seren/providers/__init__.py
@@ -0,0 +1,56 @@
+import os
+from resources.lib.common import tools
+from resources.lib.modules import database
+
+data_path = os.path.join(tools.dataPath, 'providers')
+provider_packages = [name for name in os.listdir(data_path) if os.path.isdir(os.path.join(data_path, name))]
+hoster_sources = []
+torrent_sources = []
+
+def get_relevant(language):
+    # Get relevant and enabled provider entries from the database
+    provider_status = [i for i in database.get_providers() if i['country'] == language]
+    provider_status = [i for i in provider_status if i['status'] == 'enabled']
+
+    for package in provider_packages:
+        try:
+            providers_path = 'providers.%s.%s' % (package, language)
+            provider_list = __import__(providers_path, fromlist=[''])
+            for i in provider_list.get_hosters():
+                for status in provider_status:
+                    if i == status['provider_name']:
+                        if package == status['package']:
+                            # Add import path and name to hoster_providers
+                            hoster_sources.append(('%s.hosters' % providers_path, i, package))
+
+            for i in provider_list.get_torrent():
+                for status in provider_status:
+                    if i == status['provider_name']:
+                        if package == status['package']:
+                            # Add import path and name to torrent_providers
+                            torrent_sources.append(('%s.torrent' % providers_path, i, package))
+
+        except:
+            import traceback
+            traceback.print_exc()
+            continue
+
+    return (torrent_sources, hoster_sources)
+
+def get_all(language):
+    for package in provider_packages:
+        try:
+            providers_path = 'providers.%s.%s' % (package, language)
+            provider_list = __import__(providers_path, fromlist=[''])
+            for i in provider_list.get_hosters():
+                hoster_sources.append(('%s.hosters' % providers_path, i, package))
+
+            for i in provider_list.get_torrent():
+                torrent_sources.append(('%s.torrent' % providers_path, i, package))
+
+        except:
+            import traceback
+            traceback.print_exc()
+            continue
+
+    return (torrent_sources, hoster_sources)
